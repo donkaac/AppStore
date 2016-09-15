60906d6311fb61640160cb73f410bbbd5b593223
Ish <Ish@Ishan>
2016-09-15 16:13:01 +0530

    2016/09/15:10-14AM

--- src/java/Servlets/logincheck.java
+++ src/java/Servlets/logincheck.java
@@ -11,6 +11,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -31,16 +32,12 @@ public class logincheck extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet logincheck</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet logincheck at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            HttpSession session = request.getSession();
+            session.setAttribute("uname", "ish");
+           String s= (String) session.getAttribute("uname");
+            System.out.println(s);
+            out.write(s);
+            session.invalidate();
         }
     }
 
